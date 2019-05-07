c6c006628fa02ad6143cfde0bb528edc1d7874ef
Tiago Martinho <tiagomartinho.soares@cgi.com>
2019-05-07 22:15:46 +0100

    Group movements

--- src/app/movements/movements.page.ts
+++ src/app/movements/movements.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Movement } from './movement.model';
 import { Router } from '@angular/router';
 import { MovementsService } from './movements.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-movements',
@@ -10,16 +11,17 @@ import { MovementsService } from './movements.service';
 })
 export class MovementsPage implements OnInit {
 
-  movements: Movement[] = [];
+  movementsMatrix: Movement[][] = [];
 
   constructor(private router: Router, private movementsService: MovementsService) { }
 
   ngOnInit() {
-    this.getMovements();
+
   }
 
   ionViewWillEnter() {
     this.getMovements();
+    console.log('called');
   }
 
   onNewMovement() {
@@ -27,11 +29,41 @@ export class MovementsPage implements OnInit {
   }
 
   getMovements() {
-    this.movementsService.getMovements().subscribe((res: any[]) => {
-      this.movements = res;
+    this.movementsService.getMovements().subscribe((res: Movement[]) => {
+      this.groupMovements(res);
     });
   }
 
+  //This function groups and orders the movements by year-month in order to create headers in the list presented in the view
+  groupMovements(movements: Movement[]) {
+    this.movementsMatrix = [];
+    //group by YYYY-MM
+    const groups = _.groupBy(movements, function(item) {
+      return item.date.toISOString().substring(0, 7);
+    });
+    // sort each group by date
+    // tslint:disable-next-line: forin
+    for (const key in groups) {
+      const group = groups[key].sort((n1, n2) => {
+        if (n1.date > n2.date) {
+          return -1;
+        }
 
+        if (n1 < n2) {
+          return 1;
+      }
+  
+      return 0;
+      });
+      //push array to matrix
+      this.movementsMatrix.push(group);
+    }
 
+    //sort the arrays by date
+    this.movementsMatrix = this.movementsMatrix.sort((function(a, b) { 
+      return a[6] > b[6] ? 1 : -1;
+    }));
+
+    console.log(this.movementsMatrix);
+  }   
 }
