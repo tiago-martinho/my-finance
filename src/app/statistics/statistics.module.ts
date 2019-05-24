ec8973682a7f8fe0bcc7d9dc5e36387b3e64b078
Tiago Martinho <tiagomartinho.soares@cgi.com>
2019-05-24 20:43:07 +0100

    Statistics page, phase2

--- src/app/statistics/statistics.module.ts
+++ src/app/statistics/statistics.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { StatisticsPage } from './statistics.page';
+import { ChartsModule } from 'ng2-charts';
 
 const routes: Routes = [
   {
@@ -19,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ChartsModule,
     ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
--- src/app/statistics/statistics.page.ts
+++ src/app/statistics/statistics.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-statistics',
@@ -7,6 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsPage implements OnInit {
 
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      labels: {
+        padding: 10
+      },
+      position: 'bottom',
+      
+    },
+    plugins: {
+      datalabels: {
+        formatter: (value, ctx) => {
+          const label = ctx.chart.data.labels[ctx.dataIndex];
+          return label;
+        },
+      },
+    }
+  };
+  public pieChartLabels: Label[] = ['Food', 'Home', 'Transportation', 'Gas'];
+  public pieChartData: number[] = [300, 500, 100, 400];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartColors = [
+    {
+      backgroundColor: ['#3498DB', '#FFC300', '#28B463', '#C70039', '#F5B7B1', '#9B59B6', '#196F3D', '#CD6155', '#F7DC6F', '#283747'],
+    },
+  ];
+  
   constructor() { }
 
   ngOnInit() {
