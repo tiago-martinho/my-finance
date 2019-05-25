3b18e5c9b24260b259c7e008c729b8ad86f33ac9
Tiago Martinho <tiagomartinho.soares@cgi.com>
2019-05-25 18:58:26 +0100

    Fixed some bugs in movements

--- src/app/movements/movements.page.ts
+++ src/app/movements/movements.page.ts
@@ -8,18 +8,19 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-movements',
   templateUrl: './movements.page.html',
-  styleUrls: ['./movements.page.scss'],
+  styleUrls: ['./movements.page.scss']
 })
 export class MovementsPage implements OnInit, OnDestroy {
-
-
   private movementsSub: Subscription;
   movements: Movement[];
   movementsMatrix: Movement[][] = [];
   backupMovements: Movement[][] = [];
   isLoading = false;
 
-  constructor(private router: Router, private movementsService: MovementsService) { }
+  constructor(
+    private router: Router,
+    private movementsService: MovementsService
+  ) {}
 
   ngOnInit() {
     this.movementsSub = this.movementsService.movements.subscribe(movements => {
@@ -29,15 +30,17 @@ export class MovementsPage implements OnInit, OnDestroy {
 
   ionViewWillEnter() {
     this.isLoading = true;
-    this.movementsService.getMovements().subscribe((res: Movement[]) => {
-      this.groupMovements(res);
-      this.isLoading = false;
-      this.backupMovements = this.movementsMatrix; // so we don't need to fetch the same data everytime a new search is made
-      console.log(this.backupMovements);
-    }, (error) => {
-      console.log(error);
-      this.router.navigate(['home']);
-    });
+    this.movementsService.getMovements().subscribe(
+      (res: Movement[]) => {
+        this.groupMovements(res);
+        this.isLoading = false;
+        this.backupMovements = this.movementsMatrix; // so we don't need to fetch the same data everytime a new search is made
+      },
+      error => {
+        console.log(error);
+        this.router.navigate(['home']);
+      }
+    );
   }
 
   onNewMovement() {
@@ -49,8 +52,11 @@ export class MovementsPage implements OnInit, OnDestroy {
     this.isLoading = true;
     this.movements = _.flatten(this.movementsMatrix);
     if (event.target.value !== '') {
-      this.movements = this.movements.filter(movement => movement.description.toLocaleLowerCase()
-      .includes(event.target.value.toLocaleLowerCase()));
+      this.movements = this.movements.filter(movement =>
+        movement.description
+          .toLocaleLowerCase()
+          .includes(event.target.value.toLocaleLowerCase())
+      );
       this.groupMovements(this.movements);
     }
     this.isLoading = false;
@@ -66,18 +72,18 @@ export class MovementsPage implements OnInit, OnDestroy {
 
   //This function groups and orders the movements by year-month in order to create headers in the list presented in the view
   groupMovements(movements: Movement[]) {
-
     this.movementsMatrix = [];
 
     if (movements.length === 0) {
       return;
     }
-    
+
     //group by YYYY-MM
     const groups = _.groupBy(movements, function(item) {
       return item.date.toISOString().substring(0, 7);
     });
-    // sort each group by date
+
+    // sort the movements of each group by date
     // tslint:disable-next-line: forin
     for (const key in groups) {
       const group = groups[key].sort((n1, n2) => {
@@ -87,20 +93,26 @@ export class MovementsPage implements OnInit, OnDestroy {
 
         if (n1 < n2) {
           return 1;
-      }
-  
-      return 0;
+        }
+
+        return 0;
       });
-      //push array to matrix
+
+      // push array to matrix
       this.movementsMatrix.push(group);
     }
 
-    //sort the arrays by date
-    this.movementsMatrix = this.movementsMatrix.sort((function(a, b) { 
-      return a[7] > b[7] ? 1 : -1;
-    }));
+    this.movementsMatrix = this.movementsMatrix.sort(function(a, b) {
+      return a[0].date > b[0].date ? -1 : 1;
+    });
 
-  }   
+    console.log('SORTED MATRIX ' + this.movementsMatrix);
+
+    // //sort the arrays by date
+    // this.movementsMatrix = this.movementsMatrix.sort((function(a, b) {
+    //   return a[7] > b[7] ? 1 : -1;
+    // }));
+  }
 
   ngOnDestroy(): void {
     if (this.movementsSub) {
